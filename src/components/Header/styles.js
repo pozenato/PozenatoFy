9e269db8fb5c9a7d2d9100a31f24122ad1094e5d
pozenato <bruno_pozenato@hotmail.com>
2019-08-20 00:51:01 -0300

    Initial commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 120px;
+    padding: 0 30px;
+    background: #0381FE;
+    color: #FFF;
+
+    display: flex;
+    align-items: center;
+
+`;
