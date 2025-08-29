5367610ec53213b6658ed8738799c102fde8506e
Bruno Pozenato <bruno@busify.com>
2025-08-29 11:15:17 -0300

    Atualiza configurações para deploy no GitHub Pages

---
+++ build/precache-manifest.c6fcde2e7e3f54912576685e4bcb7fbf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f599994f45f4470d4319c023cd81cf",
+    "url": "/PozenatoFy/index.html"
+  },
+  {
+    "revision": "de6ddc41a787a84fb4f3",
+    "url": "/PozenatoFy/static/js/2.be8fe22d.chunk.js"
+  },
+  {
+    "revision": "3d99ea596bfd9944fa59",
+    "url": "/PozenatoFy/static/js/main.b3e7b8a2.chunk.js"
+  },
+  {
+    "revision": "e26ec5b3202300b1303f",
+    "url": "/PozenatoFy/static/js/runtime~main.cf462775.js"
+  }
+]);
